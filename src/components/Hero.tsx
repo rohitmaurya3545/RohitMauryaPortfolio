4894800f1cc1c7775486e1d52ce7d83675934fa4
Rohit Kumar Maurya <rohitmaurya3545@gmail.com>
2025-08-28 10:29:22 +0530

    Initial commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -96,7 +96,7 @@ const Hero: React.FC = () => {
                 />
               </div>
               <div className="absolute -bottom-4 -right-4 bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg">
-                <div className="text-sm font-medium">Coder with Passion</div>
+                <div className="text-sm font-medium">Coder by Passion</div>
               </div>
             </div>
           </div>
